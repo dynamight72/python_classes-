e3597e1cc214d6dc5ec9bf2576200ba3799d0f78
ysiddhanth <turboster.flash@gmail.com>
2019-01-13 15:55:11 +0530

    first commit

---
+++ week_03/H.W.py
@@ -0,0 +1,159 @@
+#create 2 variables of each kind and convert each vriable in. total 24
+#basicvars
+#integerblue
+#bool/booleign
+#stringgreen
+#float
+
+#part 01
+
+int1 = 55
+print(int1)
+print(type(int1))
+
+int1 = "55"
+print(int1)
+print(type(int1))
+
+int1 = True
+print(int1)
+print(type(int1))
+
+int1 = 55.0
+print(int1)
+print(type(int1))
+
+
+#prt- 02
+
+int2 = 69
+print(int2)
+print(type(int2))
+
+int2 = "69"
+print(int2)
+print(type(int2))
+
+int2 = True
+print(int2)
+print(type(int2))
+
+int2 = 69.0
+print(int2)
+print(type(int2))
+
+
+#prt - 03
+
+str1 = "20"
+print(str1)
+print(type(str1))
+
+str1 = 20
+print(str1)
+print(type(str1))
+
+str1 = True
+print(str1)
+print(type(str1))
+
+str1 = 20.0
+print(str1)
+print(type(str1))
+
+
+#part 04
+
+str2 = "56"
+print(str2)
+print(type(str2))
+
+str2 = 56
+print(str2)
+print(type(str2))
+
+str2 = True
+print(str2)
+print(type(str2))
+
+str2 = 56.0
+print(str2)
+print(type(str2))
+
+
+#part 05
+bool1 = True
+print(bool1)
+print(type(bool1))
+
+bool1 = "true"
+print(bool1)
+print(type(bool1))
+
+bool1 = 1234
+print(bool1)
+print(type(bool1))
+
+bool1 = 1234.0
+print(bool1)
+print(type(bool1))
+
+
+#part 06
+
+bool2 = False
+print(bool2)
+print(type(bool2))
+
+bool2 = "false"
+print(bool2)
+print(type(bool2))
+
+bool2 = 12345
+print(bool2)
+print(type(bool2))
+
+bool2 = 12345.0
+print(bool2)
+print(type(bool2))
+
+
+#part 07
+
+float1 = 45.0
+print(float1)
+print(type(float1))
+
+float1 = 45
+print(float1)
+print(type(float1))
+
+float1 = "45"
+print(float1)
+print(type(float1))
+
+float1 = True
+print(float1)
+print(type(float1))
+
+
+#part 08
+
+float2 = 58.0
+print(float2)
+print(type(float2))
+
+float2 = 58
+print(float2)
+print(type(float2))
+
+float2 = "58"
+print(float2)
+print(type(float2))
+
+float2 = False
+print(float2)
+print(type(float2))
+
+
+
---
+++ week_03/variables_keywords.py
@@ -0,0 +1,33 @@
+print("We are in week 3")
+a = 5
+print(a)
+print(type(a))
+b = "5a"
+print(b)
+print(type(b))
+c = True
+print(c)
+print(type(c))
+d = 0.678548354
+print(d)
+print(type(d))
+
+#basicvars
+#integerblue
+#bool/booleign
+#stringgreen
+#float
+
+#part two
+#new concept-convertions
+i = 555
+print(i)
+print(float(i))
+dec = 4.5
+print(dec)
+i_dec = int(5.5)
+print(i_dec)
+s = "4545454545"
+print((s))
+print(type(s))
+
