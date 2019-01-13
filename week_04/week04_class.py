70cf226cf96826fb34701b74b21441ccaea6a368
ysiddhanth <turboster.flash@gmail.com>
2019-01-13 18:05:11 +0530

    week 4 commit

---
+++ week_04/week04_class.py
@@ -0,0 +1,50 @@
+a = 10
+b = 9000
+c = a+b
+print(c)
+
+d = 5651625
+e = 2300
+f = d-e
+print(f)
+
+g = 5
+h = 15
+i = g*h
+print(i)
+
+j = 62
+k = 5
+l = j/k
+print(l)
+
+m = 62
+n = 2
+o = m%n
+print(o)
+
+p = 15
+q = 20
+o = p**q
+print(o)
+
+s = 23
+t = 45
+u = s**t
+print(u)
+
+print(2==2)
+print(2!=563523)
+print(2<36)
+print(2>0)
+print(2<=5)
+print(2>=1)
+
+a = 10
+b = 3
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
