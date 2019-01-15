c55216c79dd2007aad2812d39908defda8755616
ysiddhanth <turboster.flash@gmail.com>
2019-01-15 11:04:52 +0530

    H.w 4w commit

--- week_04/H.W.py
+++ week_04/H.W.py
@@ -1,4 +1,5 @@
 #part 1
+print("Part One")
 n = int(input("number please"))
 print(n%2)
 if n%2<1 :
@@ -6,10 +7,45 @@ if n%2<1 :
 if n%2>0 :
         print("It is not divisible.")
 
-#part 2
+#part 02
+print("Part Two")
 n = int(input("number please"))
 print(n%6)
 if n%6<1 :
             print("It is divisible by 6.")
-if n%6>1 :
+if n%6>0 :
         print("It is not divisible.")
+
+#part 3
+print("Part Three")
+ab = int(input("Number please"))
+print(ab)
+d = int(input("Number please"))
+if ab>d :
+    print("Ab contains D.")
+if ab<d :
+    print("Ab does not contain d.")
+#part 04
+print("Part Four")
+a = int(input("Please give the number."))
+b = int(input("Please give the number."))
+ab = a+b
+print(ab)
+if ab>a**b :
+    print("Yes it is greater.")
+print(a**b)
+if ab<a**b :
+    print("No it is not greater.")
+    print(a**b)
+
+#part 05
+print("Part Five")
+a = int(input("Please give the number."))
+b = int(input("Please give the number."))
+x = int(input("Please give the number."))
+b2 = b**b
+xm = x*100<1
+if b2/xm :
+    print(b2/xm)
+    print("yeah")
+print(xm*3.068034630079427)
--- week_04/practice.py
+++ week_04/practice.py
@@ -1,2 +1,11 @@
-h = ("happy")
-print(h)
+
+print("Part Five")
+a = int(input("Please give the number."))
+b = int(input("Please give the number."))
+x = int(input("Please give the number."))
+b2 = b**b
+xm = x*100<1
+if x/b2%xm :
+    print(x/b2%xm)
+    print("yeah")
+print(xm*3.068034630079427)
