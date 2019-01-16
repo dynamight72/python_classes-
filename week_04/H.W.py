1bdb1a5ff0ce54e044da82820c405a4853ce11b1
ysiddhanth <turboster.flash@gmail.com>
2019-01-16 11:41:48 +0530

    h.w 4w commit

--- week_04/H.W.py
+++ week_04/H.W.py
@@ -43,8 +43,8 @@ print("Part Five")
 a = int(input("Please give the number."))
 b = int(input("Please give the number."))
 x = int(input("Please give the number."))
-bpowerb = b**b
-print(bpowerb)
-xm1 = bpowerb+x/2
+apowerb = b**b
+print(apowerb)
+xm1 = apowerb+x/2
 xm2 = xm1-(xm1%x)
 print(xm2)
