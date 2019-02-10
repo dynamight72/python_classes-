ea30a5422e95910aa061ecb542bc94acafeda71f
ysiddhanth <turboster.flash@gmail.com>
2019-02-10 13:10:38 +0530

    (7th week commit)

---
+++ Week_07/Week_7_homework.py
@@ -0,0 +1,87 @@
+# #Partone
+# sentence = str(input("Please give a sentence:"))
+# print(sentence.lower())
+# print(sentence.upper())
+# print(sentence.capitalize())
+#
+# #Part 2
+# hello_list = [1,2,3,5]
+# print(sum(hello_list))
+#
+# #Part 3
+# print(len(hello_list))
+#
+# #Part4
+# count = 0
+# while count <= 77:
+#     count = count+1
+#     county = sum(range(1,77))
+# print(county)
+
+
+
+# #Part five
+
+# #Part six
+# god = "The God We Believe In."
+# print(god[0:3])
+# print(god[4:7])
+# print(god[8:10])
+# print(god[11:18])
+# print(god[19:22])
+
+# def rplce(s):
+#  r = s.replace("a","!")
+
+# #part seven
+# godly = "abcdefghijklmnopqrstuvqxyz"
+# for god in godly:
+#  godly = godly.replace("abcdefghijklmnopqrstuvqxyz","!bcd!fgh!jklmn!pqrst!vqxyz")
+#
+# print(godly)
+
+# #part eight
+# inp = str(input("Please give string:"))
+# inp = inp.casefold()
+# inp_palindrome = reversed(inp)
+# if list(inp)!=list(inp_palindrome):
+#  print("It is not a palindrome.")
+# else:
+#  print("It is a palindrome.")
+# #part nine
+# def part1(s):
+#  l =  (s.lower()),(s.upper()),(s.capitalize())
+#  return l
+# def part2(s):
+#    q = sum(list(s))
+#    return q
+# def part3(s):
+#  v = len(s)
+#  return v
+# def part4(s):
+#  e = sum(range(1,s))
+#  return e
+# n = int(input("Please give number:"))
+# print(part4(n))
+# def part8(s):
+#   if list(s) == list(reversed(s)):
+#    print("It is a palindrome.")
+#   else:
+#    print("It is a not a palindrome.")
+
+# #Part ten
+# listy = ["12","35","36"]
+# print(max(listy))
+# # print(sentence.capitalize())
+# #part eleven
+# sid = str(input("Please give a string:"))
+# print("It is",sid.isalnum())
+# print("It is",sid.isalpha())
+# print("It is",sid.lower())
+# print("It is",sid.isnumeric())
+
+# #Part 12
+# string = str(input("Please give string:"))
+# print(string*30)
+
+
